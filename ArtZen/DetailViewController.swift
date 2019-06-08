b56d2bcbb3b45db92b741a75443a5be3e9d3a9ae
Kevin Jackson <kj31428@gmail.com>
2019-06-08 00:58:04 -0700

    Adds initial layout.

---
+++ ArtZen/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  ArtZen
+//
+//  Created by Kevin Jackson on 6/8/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
---
+++ ArtZen/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  ArtZen
+//
+//  Created by Kevin Jackson on 6/8/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "imageCell"
+
+class SearchViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+   
+        return cell
+    }
+}
